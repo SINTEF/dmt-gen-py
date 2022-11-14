592035d6455604b472e4d890b3b905c8c59cac8b
Lasse Bjermeland <lasse.bjermeland@sintef.no>
2022-11-14 08:36:14 +0100

    v0.3.1

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open('requirements.txt',encoding='utf8') as f:
 
 setup(
     name='dmtpygen',
-    version='0.3.0',
+    version='0.3.1',
     author="SINTEF Ocean",
     description="Python generator for SIMOS based DMT models",
     long_description=long_description,
